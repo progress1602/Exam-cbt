e0594c04bdda927b0fbf3d4f3548aa04f3d328f1
progress1602 <henrydavid1602@gmail.com>
2025-03-27 16:10:53 +0100

    removed the route in the login

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,16 @@
 "use client";
 import React from 'react';
-
-// import Login from "./components/Login";
-// import Testpart from "./components/Testpart";
 import Exam from "./components/Exam";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
 
-  useEffect(() => {
-    router.push("/login");
-  }, []);
 
   return (
     <div>
-      {/* <Login />   */}
-      {/* <Testpart /> */}
       <Exam />
     </div>
   );
-  return null; // Or a loading spinner
+  
 }
 
 
